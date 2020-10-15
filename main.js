dcfbd718689a36b5ee8f871c8d7e0ac8c4c42de9
Dikshha2601 <66013703+Dikshha2601@users.noreply.github.com>
2020-10-15 15:26:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ function preload() { }
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+     video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier(  "https://teachablemachine.withgoogle.com/models/KXzIZNJS8/model.json",model_loaded);
 }
 
-function draw(){}
+function model_loaded() {
+    window.alert("Model is loaded.");
+}
+
+function draw() {
+    frameRate(10);
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = (results[0].confidence* 100 ).toFixed(2)  + "%";
+    }
+}
